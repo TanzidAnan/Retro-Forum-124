cac2b5d0c998fbd42827debdfb944bec7b7df645
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-13 21:28:32 +0600

    local

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const loadAllPost = async (category) => {
   // console.log(category)
-  if (category) {
-
-  }
+  document.getElementById('post-container').innerHTML ='';
   const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${category ? `?category=${category}` : ''}`);
   const data = await res.json();
   displayAllPost(data.posts)
